dcb4a7c65f2d34a5bc49929ec4cf3b75b85b9f6b
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-18 17:39:20 +0530

    Time: 4 ms (92.86%), Space: 274.8 MB (7.14%) - LeetHub

--- 3809-best-reachable-tower/3809-best-reachable-tower.cpp
+++ 3809-best-reachable-tower/3809-best-reachable-tower.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
-        int distance=0;
         int maxQuality=-1;
         vector<int>bestCoordinates={-1,-1};
         for(auto& t:towers){
             int x=t[0];
             int y=t[1];
             int q=t[2];
-           distance=abs(x-center[0])+abs(y-center[1]);
+            int distance=abs(x-center[0])+abs(y-center[1]);
             if(distance<=radius){
-              if(q>maxQuality)  {
-                  maxQuality=q;
-                bestCoordinates={x,y};
-              }
-            else if(q==maxQuality){
-                if(x<bestCoordinates[0]||(x==bestCoordinates[0]&& y<bestCoordinates[1])){
+                if(q>maxQuality){
+                    maxQuality=q;
                     bestCoordinates={x,y};
                 }
-            }
-              
+                if(q==maxQuality){
+                    if(x<bestCoordinates[0]||(x==bestCoordinates[0] && y<bestCoordinates[1])){
+                        bestCoordinates={x,y};
+                    }
+                }
             }
         }
         return bestCoordinates;
