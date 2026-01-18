cfb653e52bd5974da6982e1d34060146b8c5b34c
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-18 17:43:16 +0530

    Time: 97 ms (41.67%), Space: 257.5 MB (33.33%) - LeetHub

--- 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
+++ 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& target) {
-        set<int>st;
+        unordered_set<int>st;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=target[i]){
                 st.insert(nums[i]);
