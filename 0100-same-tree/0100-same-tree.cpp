a7070437ad65093cc30cd180a5937fc1e4aa5363
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-19 14:02:51 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.4%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,14 +11,30 @@
  */
 class Solution {
 public:
-    bool dfs(TreeNode* p, TreeNode* q){
-        if(!p && !q)return true;
-        if(!p||!q)return false;
-        if(p->val!=q->val)return false;
-        return dfs(p->left,q->left)&&
-        dfs(p->right,q->right);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-      return  dfs(p,q);
+        if(q==NULL||p==NULL)return p==q;
+        if(p->val!=q->val)return false;
+        return isSameTree(p->left,q->left)&&
+        isSameTree(p->right,q->right);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
