2067e1725afa0331edd8f8c3774f4d2e8c41f9d1
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-19 22:20:24 +0530

    Time: 3 ms (8.54%), Space: 24.3 MB (66.44%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,22 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-int count=0,ans=0;
-int inorder(TreeNode* node,int k){
-    if(!node)return NULL;
-    inorder(node->left,k);
-    count++;
-    if(count==k)ans=node->val;
-    inorder(node->right,k);
-    return ans;
-}
+    int count = 0, ans = 0;
+
+    void inorder(TreeNode* node, int k) {
+        if (!node) return;
+
+        inorder(node->left, k);
+
+        count++;
+        if (count == k) {
+            ans = node->val;
+            return; // stop early once kth element found
+        }
+
+        inorder(node->right, k);
+    }
+
     int kthSmallest(TreeNode* root, int k) {
-        return inorder(root,k);
-      
+        count = 0;
+        ans = 0;
+        inorder(root, k);
+        return ans;
     }
 };
