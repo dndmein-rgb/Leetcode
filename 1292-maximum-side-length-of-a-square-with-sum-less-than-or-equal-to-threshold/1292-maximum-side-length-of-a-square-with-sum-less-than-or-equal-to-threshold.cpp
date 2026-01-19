06d337f919e79361c91b50f34c0010c216314a1f
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-19 21:45:38 +0530

    Time: 16 ms (45.45%), Space: 32.7 MB (78.18%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
-    bool check( int k,int i, int j,  int threshold,vector<vector<int>>& prefix) {
-        int n = prefix.size()-1;
-        int m = prefix[0].size()-1;
-
-        if(i+k> n || j+k>m){
-            return false;
-        }
-        int sum = prefix[i+k][j+k]-prefix[i+k][j-1]-prefix[i-1][j+k]+prefix[i-1][j-1];
-        return sum<=threshold;
+    bool canBuild(int len, int i, int j, int threshold, vector<vector<int>>& pref) {
+        int sum = pref[i + len - 1][j + len - 1]
+                - pref[i - 1][j + len - 1]
+                - pref[i + len - 1][j - 1]
+                + pref[i - 1][j - 1];
+        return sum <= threshold;
     }
+
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int n = mat.size();
-        int m = mat[0].size();
-        int maxi = 0;
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> pref(n + 1, vector<int>(m + 1, 0));
 
-        vector<vector<int>> prefix(n+1,vector<int>(m+1,0));
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                prefix[i][j]=mat[i-1][j-1];
-                prefix[i][j]+=prefix[i-1][j];
-                prefix[i][j]+=prefix[i][j-1];
-                prefix[i][j]-=prefix[i-1][j-1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                pref[i][j] = mat[i - 1][j - 1]
+                           + pref[i - 1][j]
+                           + pref[i][j - 1]
+                           - pref[i - 1][j - 1];
             }
         }
 
-        
+        int ans = 0;
 
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                int low =maxi;
-                int high = min(n,m);
+                int low = ans + 1;
+                int high = min(n - i + 1, m - j + 1);
 
-                while(low<=high){
-                    int mid =(low+high)/2;
-                    if(check(mid,i,j,threshold,prefix)){
-                        low =mid+1;
-                        maxi =max(maxi,mid+1);
-                    }else{
-                        high =mid-1;
+                while (low <= high) {
+                    int mid = (low + high) / 2;
+                    if (canBuild(mid, i, j, threshold, pref)) {
+                        ans = mid;
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
                     }
                 }
             }
         }
-
-        return maxi;
+        return ans;
     }
 };
