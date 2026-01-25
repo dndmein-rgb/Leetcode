844c9bcf1f64998dd419199b578b631d6d31a8fb
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-25 11:29:13 +0530

    Time: 4 ms (23.72%), Space: 17.2 MB (56.92%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
+        if (k == 1) return 0;
+
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int minDiff=1e9;
-        if (n == 1)
-            return 0;
-      
+        int minDiff = INT_MAX;
+
         for (int i = 0; i + k - 1 < n; i++) {
             minDiff = min(minDiff, nums[i + k - 1] - nums[i]);
         }
-    return minDiff;
+
+        return minDiff;
     }
 };
