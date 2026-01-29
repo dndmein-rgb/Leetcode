7e5a2bde8f0ad232bffe84e351e8fcc22f190367
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-29 23:36:28 +0530

    Time: 56 ms (36.26%), Space: 106.8 MB (24.71%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    void dijkstra(int source, vector<vector<pair<int,int>>>& adj,
-                  vector<vector<int>> &dist) {
-        dist[source][source] = 0;
-        priority_queue<pair<int, int>, vector< pair<int, int>>,
-            greater < pair < int, int >>> pq;
-        pq.push({0, source});
-        while (!pq.empty()) {
-            int d=pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            if (d > dist[source][node]) continue;
-            for (auto i : adj[node]){
-                int ne=i.second;
-                int wt=i.first;
-                if(dist[source][ne]>dist[source][node]+wt){
-                      dist[source][ne]=dist[source][node]+wt;
-                    pq.push({dist[source][ne],ne});
-                }
+void dijkstra(int source, vector<vector<pair<int,int>>>&adj, vector<vector<int>>&dist){
+    dist[source][source]=0;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+    pq.push({0,source});
+    while(!pq.empty()){
+        auto[d,u]=pq.top();
+        pq.pop();
+        if(d>dist[source][u])continue;
+        for(auto& i :adj[u]){
+            int cost=i.first;
+            int v=i.second;
+            if(dist[source][v]>dist[source][u]+cost){
+                dist[source][v]=dist[source][u]+cost;
+                pq.push({dist[source][v],v});
             }
         }
     }
-    long long minimumCost(string source, string target, vector<char>& original,
-                          vector<char>& changed, vector<int>& cost) {
-         vector<vector<pair<int,int>>> adj(26);
-        for (int i = 0; i < original.size(); i++) {
-            int u = original[i] - 'a';
-            int v = changed[i] - 'a';
-            adj[u].push_back({cost[i], v});
+}
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<pair<int,int>>>adj(26);
+        for(int i=0;i<original.size();i++){
+            int u=original[i]-'a';
+            int v=changed[i]-'a';
+            adj[u].push_back({cost[i],v});
         }
-        vector<vector<int>> dist(26, vector<int>(26, 1e9));
-        for (int i = 0; i < 26; i++) {
-            dijkstra(i, adj, dist);
+        vector<vector<int>>dist(26,vector<int>(26,1e9));
+        for(int i=0;i<26;i++){
+            dijkstra(i,adj,dist);
         }
-
-        long long ans = 0;
-        for (int i = 0; i < source.size(); i++) {
-            int u = source[i] - 'a';
-            int v = target[i] - 'a';
-            if (dist[u][v] != 1e9)
-                ans += dist[u][v];
-            else
-                return -1;
+        long long ans=0;
+        for(int i=0;i<source.size();i++){
+            int u=source[i]-'a';
+            int v=target[i]-'a';
+            if(dist[u][v]!=1e9)ans+=dist[u][v];
+            else return -1;
         }
         return ans;
     }
