0d17cfe5243f51ee2a31704e614350e99aaf1002
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-05 09:31:59 +0530

    Time: 8 ms (46.52%), Space: 25.6 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size();
         vector<int>result(n,0);
         for(int i=0;i<n;i++){
-            if(nums[i]>0)result[i]=nums[(i+nums[i])%n];
-            else if(nums[i]<0)result[i]=nums[abs(i+n+nums[i]%n)%n];
+            // if(nums[i]>0)result[i]=nums[(i+nums[i])%n];
+           result[i]=nums[abs(i+n+nums[i]%n)%n];
         }
         return result;
     }
