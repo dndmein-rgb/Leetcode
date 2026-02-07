07cc0c8aa9ac4a7b8a0c0e7678719ff2c4797a0a
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-07 11:39:39 +0530

    Time: 29 ms (50.44%), Space: 26.4 MB (44.82%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        vector<int> prefB(n+1, 0);
-        vector<int> suffA(n+1, 0);
-        for (int i = 1; i <= n; i++) {
-            if (s[i - 1] == 'b') {
-                prefB[i] = prefB[i - 1] + 1;
-            } else
-                prefB[i] = prefB[i - 1];
+        int n=s.length();
+        int count=0;
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(!st.empty() && s[i]=='a' && st.top()=='b'){
+                st.pop();
+                count++;
+            }
+            else st.push(s[i]);
+        
         }
-    
-    for (int i = n - 1; i >= 0; i--) {
-        if (s[i+1]=='a')suffA[i]=suffA[i+1]+1;
-        else suffA[i]=suffA[i+1];
-    }
-    int lastIdx=0;
-    int ans = n;
-    for (int i = 0; i <= n; i++)
-        ans = min(ans, prefB[i] + suffA[i]);
-
-    return ans;
+        return count;
     }
 };
