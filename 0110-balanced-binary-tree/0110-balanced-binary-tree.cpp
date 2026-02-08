37d8b5fdf42d000ada1a3da07bec7cf31b372d6c
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-08 09:23:37 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.68%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,16 +12,16 @@
 class Solution {
 public:
     int dfs(TreeNode* node){
-        if(!node) return 0;
+        if(!node)return 0;
         int left=dfs(node->left);
         if(left==-1)return -1;
         int right=dfs(node->right);
         if(right==-1)return -1;
         if(abs(left-right)>1)return -1;
         return 1+max(left,right);
-
+        
     }
     bool isBalanced(TreeNode* root) {
-       return dfs(root)!=-1;
+        return dfs(root)!=-1;
     }
 };
