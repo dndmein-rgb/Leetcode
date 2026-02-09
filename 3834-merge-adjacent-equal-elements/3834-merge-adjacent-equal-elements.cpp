81c64da5624141d78404175f6f6de990e83269bd
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-09 22:37:57 +0530

    Time: 14 ms (92.31%), Space: 199.2 MB (100%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<long long> mergeAdjacent(vector<int>& nums) {
-        int n=nums.size();
-        vector<long long >ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            long long val=nums[i];
-            while(ans.size()>0 && ans.back()==val){
-                ans.pop_back();
-                val=val*2;
-            }
-            ans.push_back(val);
+      int n=nums.size()  ;
+      vector<long long >ans;
+      ans.push_back(nums[0]);
+      for(int i=1;i<n;i++){
+        long long val=nums[i];
+        while(ans.size()>0 && val==ans.back()){
+            ans.pop_back();
+            val*=2;
         }
-        return ans;
-
+        ans.push_back(val);
+      }
+      return ans;
     }
 };
