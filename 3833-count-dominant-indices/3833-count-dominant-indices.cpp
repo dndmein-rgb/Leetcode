da20642d3463fed6aaad3156d02d1a2e2506fd4d
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-09 22:27:07 +0530

    Time: 0 ms (100%), Space: 32.2 MB (0%) - LeetHub

--- 3833-count-dominant-indices/3833-count-dominant-indices.cpp
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int dominantIndices(vector<int>& nums) {
-        int n=nums.size();  
-        int sums=nums[n-1],count=0;
+        int n=nums.size();
+        int sums=nums[n-1];
+        int count=0;
         for(int i=n-2;i>=0;i--){
-           
             if(nums[i]>sums/(n-i-1))count++;
-             sums+=nums[i];
+            sums+=nums[i];
         }
         return count;
     }
