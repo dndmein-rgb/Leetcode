50667d59beb7fe577a201c0f027155ebfb0c1847
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-12 10:04:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int longestBalanced(string s) {
-        int n=s.size();
         int maxi=0;
+        int n=s.length();
         for(int i=0;i<n;i++){
             vector<int>freq(26,0);
             for(int j=i;j<n;j++){
                 freq[s[j]-'a']++;
-                int tar=freq[s[j]-'a'];
+                int target=freq[s[j]-'a'];
                 bool flag=true;
                 for(int k=0;k<26;k++){
-                    if(freq[k]>0 &&freq[k] !=tar){
+                    if(freq[k]>0 && freq[k]!=target ){
                         flag=false;
                         break;
                     }
                 }
-                if(flag){
-                    maxi=max(maxi,j-i+1);
-                }
+                    if(flag){
+                        maxi=max(maxi,target);
+                    }
+                
+                
             }
         }
         return maxi;
+
     }
 };
