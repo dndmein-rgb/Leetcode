03406be79257be7839f7bc4d0b2de4488f1f3a8b
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-16 23:33:18 +0530

    Time: 28 ms (13.04%), Space: 254.1 MB (8.7%) - LeetHub

---
+++ 3840-house-robber-v/3840-house-robber-v.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+vector<long long>dp;
+    long long  solve(int i,vector<int>& nums, vector<int>& colors){
+        int n=nums.size();
+        if(i>=n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        long long skip=solve(i+1,nums,colors);
+        long long take;
+        if(i+1<n && colors[i]==colors[i+1]){
+            take=nums[i]+solve(i+2,nums,colors);
+        }else{
+            take=nums[i]+solve(i+1,nums,colors);
+        }
+        return dp[i]=max(take,skip);
+    }
+    long long rob(vector<int>& nums, vector<int>& colors) {
+        int n=nums.size();
+        dp.assign(n,-1);
+        return solve(0,nums,colors);
+    }
+};
