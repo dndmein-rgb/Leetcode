2a90d3ff7422728cfb5af7e40e4208df30996193
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-18 23:11:01 +0530

    Time: 443 ms (63.31%), Space: 26.5 MB (52.34%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-bool isPrime(int n){
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0)return false;
-    }
-    return true;
-}
+
     int largestPrime(int n) {
         if(n==1)return false;
-         if(n>=398771)return 398771;
-        vector<int>primes;
-        for(int i=2;i<=n;i++){
-            if(isPrime(i))primes.push_back(i);
+        //  if(n>=398771)return 398771;
+        vector<bool>isPrime(n+1,true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j=i*i ;j<=n;j+=i){
+                    isPrime[j]=false;
+                }
+            }
         }
         int sum=0,ans=0;
-        for(int x:primes){
-            sum+=x;
+       for(int i=2;i<=n;i++){
+        if(isPrime[i]){
+            sum+=i;
             if(sum>n)break;
-            if(isPrime(sum))ans=sum;
+            if(isPrime[sum])ans=sum;
         }
+       }
         return ans;
     }
 };
