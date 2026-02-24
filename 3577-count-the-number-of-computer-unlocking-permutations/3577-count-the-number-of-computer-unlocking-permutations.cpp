004575f2629932c23d2002060686af67d52b76a6
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-24 20:32:06 +0530

    Time: 0 ms (100%), Space: 92.8 MB (36.68%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int mod = 1e9+7;
     int countPermutations(vector<int>& complexity) {
-        int val = complexity[0];
-        int n = complexity.size();
-        for(int i=1; i<n; i++) {
-            if(complexity[i] <= val) return 0;
+        int n=complexity.size();
+        const int mod=1e9+7;
+        int ans=1;
+        for(int i=1;i<n;i++){
+            if(complexity[i]<=complexity[0])return 0;
+            ans=(ans*1LL*i)%mod;
         }
-
-        int ans = 1;
-        for(int i=1; i<=n-1; i++) {
-            ans = (ans*1LL*i)%mod;
-        }
-
         return ans;
     }
 };
