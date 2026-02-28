5af1fd967e57c0526f102a879735eb8ee25e2280
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-28 10:35:07 +0530

    Time: 49 ms (47.65%), Space: 8.2 MB (21.48%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
-const int mod=1e9+7;
-    string binaryRep(int n){
-        string s;
-        while(n){
-            int digit=n&1;
-            s.push_back(digit+'0');
-            n>>=1;
-        }
-        reverse(s.begin(),s.end());
-        return s;
-    }
-    int binaryToDecimal(string s){
-        int result=0;
-        for(char c:s){
-            result=(result*2+(c-'0'))%mod;
-        }
-        return result;
-    }
     int concatenatedBinary(int n) {
-        string s;
+        long num=0;
+        int mod=1e9+7;
         for(int i=1;i<=n;i++){
-            s+=binaryRep(i);
+            int digits=log2(i)+1;
+            num=(((num<<digits)%mod)+i)%mod;
         }
-        int size=s.length();
-        int j=size-1;
-        int num=binaryToDecimal(s);
         return num;
-        
     }
 };
