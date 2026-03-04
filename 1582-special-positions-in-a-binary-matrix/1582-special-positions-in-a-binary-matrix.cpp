00c6c70c251b33c4811266d2d07030c4b6fac12b
Divyanshu Rathore <dnd.mein@gmail.com>
2026-03-04 08:20:03 +0530

    Time: 3 ms (19.5%), Space: 17 MB (41.15%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -4,24 +4,19 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         int count = 0;
+        vector<int>rowCount(n,0),colCount(m,0);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 1) {
-                    bool flag = true;
-                    for (int k = 0; k < m; k++) {
-                        if (j != k && mat[i][k] == 1) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    for (int l = 0; l < n; l++) {
-                        if (i != l && mat[l][j] == 1) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag)
-                        count++;
+                if(mat[i][j]==1){
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(mat[i][j]==1 && rowCount[i]==1 && colCount[j]==1){
+                  count++;
                 }
             }
         }
