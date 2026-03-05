0195c24ab76e1c750a66e9e301faf1d2cac11c01
Divyanshu Rathore <dnd.mein@gmail.com>
2026-03-05 12:09:54 +0530

    Time: 0 ms (100%), Space: 9.6 MB (20.96%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     int minOperations(string s) {
         int n=s.length();
-        string t=s;
-        int count1=0,count2=0;
-        for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                count1++;
-                if(s[i]=='1')s[i+1]='0';
-                else s[i+1]='1';
+        int start_with_zero=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                if(s[i]=='1')start_with_zero++;
+            }
+            else {
+                if(s[i]=='0')start_with_zero++;
             }
         }
-        if(t[0]=='0')t[0]='1';
-        else t[0]='0';
-        for(int i=0;i<n-1;i++){
-            if(t[i]==t[i+1]){
-                count2++;
-                if(t[i]=='1')t[i+1]='0';
-                else t[i+1]='1';
-            }
-        }
-    return min(count1,count2+1);
+        return min(start_with_zero,n-start_with_zero);
     }
 };
