fa561778be40f0c2c72e6d4ae78eb5d36a92be02
Divyanshu Rathore <dnd.mein@gmail.com>
2026-03-08 10:57:15 +0530

    Time: 0 ms (100%), Space: 12.6 MB (86.66%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,42 +1,11 @@
 class Solution {
 public:
-    int binaryToDecimal(string s){
-        int num = 0;
-        for(char c : s){
-            num = num*2 + (c-'0');
-        }
-        return num;
-    }
-
-    string decToBinary(int n, int bits){
-        string s;
-        while(n){
-            s.push_back(n%2 + '0');
-            n/=2;
-        }
-        while(s.size() < bits) s.push_back('0');
-        reverse(s.begin(), s.end());
-        return s;
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums.size();
-
-        vector<bool> vis(1<<n,false);
-
-        for(auto &s:nums){
-            int value=binaryToDecimal(s);
-            vis[value]=true;
+        int n=nums.size();
+        string ans;
+        for(int i=0;i<n;i++){
+            ans.push_back((nums[i][i]=='0')?'1':'0');
         }
-
-        int ans=0;
-        for(int i=0;i<(1<<n);i++){
-            if(!vis[i]){
-                ans=i;
-                break;
-            }
-        }
-
-        return decToBinary(ans,n);
+        return ans;
     }
 };
