84a4ea368cdb28ad0a5c906295d0663eb5842400
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-10 20:22:52 +0530

    Time: 17 ms (44.15%), Space: 252.3 MB (40.08%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.cpp
+++ 3840-house-robber-v/3840-house-robber-v.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-vector<long long >dp;
-    long long solve(int i,vector<int>& nums, vector<int>& colors){
-        int n=nums.size();
-        if(i>=n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        long long skip=solve(i+1,nums,colors);
-        long long take=-1e9;
-        if(i+1<n && colors[i]==colors[i+1]){
-            take=nums[i]+solve(i+2,nums,colors);
-        }else {
-            take=nums[i]+solve(i+1,nums,colors);
-        }
-        return dp[i]=max(take,skip);
+vector<long long>dp;
+int n;
+long long solve(int i,vector<int>& nums, vector<int>& colors){
+    if(i==n)return 0;
+    if(dp[i]!=-1)return dp[i];
+    long long  take=-1e9;
+    if(i+1<n && colors[i]==colors[i+1]){
+        take=nums[i]+solve(i+2,nums,colors);
+    }else {
+        take=nums[i]+solve(i+1,nums,colors);
     }
+    long long notTake=solve(i+1,nums,colors);
+    return dp[i]=max(take,notTake);
+}
     long long rob(vector<int>& nums, vector<int>& colors) {
-        int n=nums.size();
+         n=nums.size();
         dp.assign(n,-1);
         return solve(0,nums,colors);
     }
-}; 
+};
