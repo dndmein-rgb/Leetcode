5013f85cf7b669bdf223e5ec720f2958721315f5
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-10 23:19:47 +0530

    Time: 51 ms (80.49%), Space: 185.8 MB (91.05%) - LeetHub

--- 3835-count-subarrays-with-cost-less-than-or-equal-to-k/3835-count-subarrays-with-cost-less-than-or-equal-to-k.cpp
+++ 3835-count-subarrays-with-cost-less-than-or-equal-to-k/3835-count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -3,19 +3,17 @@ public:
     long long countSubarrays(vector<int>& nums, long long k) {
         int n = nums.size();
         deque<int> maxDq, minDq;
-        long long ans = 0;
-        int l=0;
+        int l = 0;
+        long long  ans=0;
         for (int r = 0; r < n; r++) {
             while (!maxDq.empty() && nums[maxDq.back()] <= nums[r])
                 maxDq.pop_back();
             maxDq.push_back(r);
-
             while (!minDq.empty() && nums[minDq.back()] >= nums[r])
                 minDq.pop_back();
             minDq.push_back(r);
-
             while (!maxDq.empty() && !minDq.empty() &&
-                   (long long)(nums[maxDq.front()] - nums[minDq.front()]) * (r-l+1)>k){
+                   (  (long long)(nums[maxDq.front()] - nums[minDq.front()]) * (r - l + 1))>k){
                     if(maxDq.front()==l)maxDq.pop_front();
                     if(minDq.front()==l)minDq.pop_front();
                     l++;
