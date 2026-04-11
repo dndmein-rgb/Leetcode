b8c818386e2ebf0697e2219708ef9b7ce2325ccb
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-11 15:37:39 +0530

    Time: 66 ms (70.06%), Space: 197.5 MB (68.57%) - LeetHub

--- 3824-minimum-k-to-reduce-array-within-limit/3824-minimum-k-to-reduce-array-within-limit.cpp
+++ 3824-minimum-k-to-reduce-array-within-limit/3824-minimum-k-to-reduce-array-within-limit.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-long long nonPositives(vector<int>& nums,int k){
-    long long totalOps=0;
-    for(int num:nums){
-        totalOps+=((num+k-1)/k);
+    bool isValid(vector<int>& nums, long long k) {
+        long long ops = 0;
+        long long limit = k * k;
+
+        for (int num : nums) {
+            ops += (num + k - 1) / k;  // ceil division
+
+            if (ops > limit) return false; // early exit
+        }
+        return ops <= limit;
     }
-    return totalOps;
-}
+
     int minimumK(vector<int>& nums) {
-        long long  low=1,high=100000;
-        long long ans=0;
-        while(low<=high){
-            long long  mid=low+(high-low)/2;
-            long long limit=mid*mid;
-            if(nonPositives(nums,mid)<=limit){
-                ans=mid;
-                high=mid-1;
-            }else low=mid+1;
+        long long low = 1, high = 100000;
+        long long ans = high;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (isValid(nums, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
         return ans;
     }
