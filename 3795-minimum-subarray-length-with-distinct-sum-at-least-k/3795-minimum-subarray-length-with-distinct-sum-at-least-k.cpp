92de3ed7fd4d95d0f062eab09908a9391aef83fe
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-12 00:33:20 +0530

    Time: 436 ms (21.41%), Space: 317.9 MB (28.45%) - LeetHub

---
+++ 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minLength(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        int i=0,j=0;
+        int sum=0;
+        int minLen=n+1;
+        while(j<n){
+            if(!mp.count(nums[j])){
+                sum+=nums[j];
+            }
+            mp[nums[j]]++;
+             if(sum>=k)minLen=min(minLen,j-i+1);
+            while(sum>=k && i<=j){
+                
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0){
+                    mp.erase(nums[i]);
+                    sum-=nums[i];
+                }
+                i++;
+                if(sum>=k)minLen=min(minLen,j-i+1);
+            }
+            if(sum>=k)minLen=min(minLen,j-i+1);
+            j++;
+        }
+        return minLen==n+1?-1:minLen;
+    }
+};
