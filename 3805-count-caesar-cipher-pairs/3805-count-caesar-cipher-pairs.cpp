96490a699516be07fa21fdf8323366f69f08b464
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-11 19:00:51 +0530

    Time: 20 ms (91.57%), Space: 107.3 MB (95.7%) - LeetHub

--- 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.cpp
+++ 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     long long countPairs(vector<string>& words) {
-        unordered_map<string, long long> freq;
-        long long ans = 0;
-        for (auto& w : words) {
-            int diff = w[0]-'a';
-            for (auto& c : w) {
-                c -= diff;
-                if (c < 'a') c += 26;
+        unordered_map<string,long long >freq;
+        long long ans=0;
+        for(string& w:words){
+            int diff=w[0]-'a';
+            for(char& c:w){
+                c-=diff;
+                if(c<'a')c+=26;
+                
             }
-            ans += freq[w];
+            ans+=freq[w];
             freq[w]++;
         }
         return ans;
