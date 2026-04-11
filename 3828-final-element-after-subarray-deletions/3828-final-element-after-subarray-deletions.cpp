331e53e28c2374b8adc076fe5cffa1e0d3e03b50
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-11 14:43:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3828-final-element-after-subarray-deletions/3828-final-element-after-subarray-deletions.cpp
+++ 3828-final-element-after-subarray-deletions/3828-final-element-after-subarray-deletions.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int finalElement(vector<int>& nums) {
-        int n=nums.size();
-    //    if(n==1) return nums[0];
-       return max(nums[0],nums[n-1]);
+       return max(nums[.front(),nums.back());
     }
 };
