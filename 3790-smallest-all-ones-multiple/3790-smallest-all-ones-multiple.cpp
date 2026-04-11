5132f97a049294325610795fe7156382785ab9ea
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-12 01:17:56 +0530

    Time: 10 ms (47.02%), Space: 8.5 MB (88.1%) - LeetHub

--- 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.cpp
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minAllOneMultiple(int k) {
-        if(k%2==0 ||k%5==0)return -1;
+        if((k%2==0) || (k%5==0))return -1;
         long long num=1;
         int count=1;
         while(count<=k){
             num=(num*10+1)%k;
             count++;
             if(num%k==0)return count;
-        }return -1;
+        }
+        return -1;
     }
 };
