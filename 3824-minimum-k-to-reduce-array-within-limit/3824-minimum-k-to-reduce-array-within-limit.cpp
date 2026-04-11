e290077df515e91d7c8318eab59b8439dd48a1c2
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-11 15:33:41 +0530

    Time: 47 ms (96.47%), Space: 197.5 MB (68.57%) - LeetHub

--- 3824-minimum-k-to-reduce-array-within-limit/3824-minimum-k-to-reduce-array-within-limit.cpp
+++ 3824-minimum-k-to-reduce-array-within-limit/3824-minimum-k-to-reduce-array-within-limit.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int countOperations(vector<int>&nums,int mid){
-        int operations=0;
-        for(int num:nums){
-            operations+=(num+mid-1)/mid;
-        }
-        return operations;
+long long nonPositives(vector<int>& nums,int k){
+    long long totalOps=0;
+    for(int num:nums){
+        totalOps+=((num+k-1)/k);
     }
+    return totalOps;
+}
     int minimumK(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        long long  l = 1, r = 100000;
-        int ans=nums[0];
-        while (l<=r){
-            long long  mid=l+(r-l)/2;
-            if(countOperations(nums,mid)<=mid*mid){
+        long long  low=1,high=100000;
+        long long ans=0;
+        while(low<=high){
+            long long  mid=low+(high-low)/2;
+            long long limit=mid*mid;
+            if(nonPositives(nums,mid)<=limit){
                 ans=mid;
-                r=mid-1;
-            }
-            else l=mid+1;
+                high=mid-1;
+            }else low=mid+1;
         }
         return ans;
     }
