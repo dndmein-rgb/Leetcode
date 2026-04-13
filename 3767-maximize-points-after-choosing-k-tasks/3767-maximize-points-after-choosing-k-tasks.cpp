dd2c3e3edf09feea883e4f326ab4269ba8b1cd24
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-13 12:06:54 +0530

    Time: 159 ms (16.11%), Space: 231.5 MB (20.55%) - LeetHub

--- 3767-maximize-points-after-choosing-k-tasks/3767-maximize-points-after-choosing-k-tasks.cpp
+++ 3767-maximize-points-after-choosing-k-tasks/3767-maximize-points-after-choosing-k-tasks.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     long long maxPoints(vector<int>& technique1, vector<int>& technique2, int k) {
         int n = technique1.size();
+        vector<int> dist(n);
+        for(int i = 0; i < n; i++) {
+            dist[i] = technique1[i] - technique2[i];
+        }
+
         priority_queue<pair<int, int>> pq;
-        
-        for (int i = 0; i < n; i++) {
-            // Priority queue helps us find which tasks are "best" for technique1
-            pq.push({technique1[i] - technique2[i], i});
+        for(int i = 0; i < n; i++) {
+            pq.push({dist[i], i});
         }
 
         long long totalSum = 0;
-        int countTech1 = 0;
-
         while (!pq.empty()) {
-            auto [diff, i] = pq.top();
+            auto [val, i] = pq.top();
             pq.pop();
 
-            // 1. We MUST pick technique1 for the first k items we pull from the heap
-            // because these are the items where technique1 performs best relative to tech2.
-            if (countTech1 < k) {
+           
+            if (k > 0) {
                 totalSum += technique1[i];
-                countTech1++;
+                k--;
             } 
-            // 2. For the rest, we have met our requirement, so just pick the maximum.
+            
             else {
                 totalSum += max(technique1[i], technique2[i]);
             }
