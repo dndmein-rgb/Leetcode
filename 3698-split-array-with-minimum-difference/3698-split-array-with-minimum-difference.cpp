92b26910ec94bc2a24b827a936ac64e9da45f071
Divyanshu Rathore <dnd.mein@gmail.com>
2026-04-15 15:00:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3698-split-array-with-minimum-difference/3698-split-array-with-minimum-difference.cpp
+++ 3698-split-array-with-minimum-difference/3698-split-array-with-minimum-difference.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     long long splitArray(vector<int>& nums) {
-        int n = nums.size();
-        if (n < 2) return -1;
-
-        vector<bool> inc(n, false);
-        vector<bool> dec(n, false);
-
-        // strictly increasing from left
-        inc[0] = true;
-        for (int i = 1; i < n; i++) {
-            inc[i] = inc[i - 1] && (nums[i] > nums[i - 1]);
+        int n=nums.size();
+        vector<bool>inc(n,false),dec(n,false);
+        inc[0]=true;
+        for(int i=1;i<n;i++){
+            inc[i]=inc[i-1] && (nums[i]>nums[i-1]);
         }
-
-        // strictly decreasing from right
-        dec[n - 1] = true;
-        for (int i = n - 2; i >= 0; i--) {
-            dec[i] = dec[i + 1] && (nums[i] > nums[i + 1]);
+        dec[n-1]=true;
+        for(int i=n-2;i>=0;i--){
+            dec[i]=dec[i+1] && (nums[i]>nums[i+1]);
         }
-
-        // prefix sums
-        vector<long long> pref(n);
-        pref[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1] + nums[i];
+        vector<int>pref(n,0);
+        pref[0]=nums[0];
+        long long total=nums[0];
+        for(int i=1;i<n;i++){
+            pref[i]=pref[i-1]+nums[i];
+            total+=nums[i];
         }
-
-        long long total = pref[n - 1];
-        long long ans = LLONG_MAX;
-        bool found = false;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (inc[i] && dec[i + 1]) {
-                long long leftSum = pref[i];
-                long long rightSum = total - leftSum;
-                ans = min(ans, llabs(leftSum - rightSum));
-                found = true;
+        long long minD=LLONG_MAX;
+        bool found=false;
+        for(int i=0;i<n;i++){
+            if(inc[i] && dec[i+1]){
+                long long left=pref[i];
+                long long right=total-left;
+                minD=min(minD,llabs(left-right));
+                found=true;
             }
         }
+        return found?minD:-1;
 
-        return found ? ans : -1;
     }
 };
