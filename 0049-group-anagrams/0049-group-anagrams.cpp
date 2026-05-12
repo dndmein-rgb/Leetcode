aea05e769fb1945a6a28677f9722d9d4b8c6f8fb
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-12 12:26:42 +0530

    Time: 12 ms (82.74%), Space: 25 MB (78.45%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(auto & s:strs){
+            string sorted=s;
+            sort(sorted.begin(),sorted.end());
+            mp[sorted].push_back(s);
+        }
+        vector<vector<string>>ans;
+        for(auto& it:mp){
+           
+            ans.push_back(it.second);
+           
+        }
+        return ans;
+    }
+};
