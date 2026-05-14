f61b286eb89afa7430d1998bc576d24ae7a653bb
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-14 23:46:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int i=0,j=m*n-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            int r=mid/n-1;
+            int c=mid%m;
+            if(matrix[r][c]==target)return true;
+            else if(matrix[r][c]<target)i++;
+            else j--;
+        }
+        return false;
+    }
+};
