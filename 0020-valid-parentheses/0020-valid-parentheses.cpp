ecd713414e4567e270a9f511b2eed0cef8fe9c0c
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-14 15:28:03 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (st.empty() && (c == ')' || c == ']' || c == '}'))
+                return false;
+            if (c == '(' || c == '{' || c == '[')
+                st.push(c);
+            else if ((c == ')' && st.top() == '(') ||
+                     (c == ']' && st.top() == '[') ||
+                     (c == '}' && st.top() == '{'))
+                     st.pop();
+                     else return false;
+        }
+        return st.empty() ?true :false;
+    }
+};
