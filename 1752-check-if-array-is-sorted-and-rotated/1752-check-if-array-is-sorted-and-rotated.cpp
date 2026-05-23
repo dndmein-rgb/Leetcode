56dd493bc9d7d4fbd1e0e360570791d1ee955d2c
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-23 11:45:26 +0530

    Time: 0 ms (100%), Space: 11.2 MB (82.15%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,20 +4,12 @@ public:
 
         int n = nums.size();
 
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
+       int count=0;
+       for(int i=0;i<n;i++){
+        if(nums[i]>nums[(i+1)%n])count++;
+       }
+        return count<=1;
 
-        for(int i = 0; i < n; i++) {
-
-            vector<int> test = sorted;
-
-            rotate(test.begin(), test.begin() + i, test.end());
-
-            if(test == nums) {
-                return true;
-            }
-        }
-
-        return false;
+        
     }
 };
