7811be2778df1e3dbedc8b2ec998756b8d111cd1
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-23 16:32:36 +0530

    Time: 0 ms (100%), Space: 19.9 MB (38.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        vector<int>indegree(numCourses,0);
-        for(auto& e:prerequisites){
-            adj[e[1]].push_back(e[0]);
-            indegree[e[0]]++;
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(numCourses, 0);
+        for (auto& q : prerequisites) {
+            int u = q[0];
+            int v = q[1];
+            adj[v].push_back(u);
+            indegree[u]++;
         }
-        queue<int>q;
-        int count=0;
-        for(int i=0;i<numCourses;i++){
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
             if(indegree[i]==0)q.push(i);
         }
+        int count=0;
         while(!q.empty()){
             int node=q.front();
             q.pop();
             count++;
-
             for(int nei:adj[node]){
                 indegree[nei]--;
-                if(indegree[nei]==0){
-                    q.push(nei);
-                }
+                if(indegree[nei]==0)q.push(nei);
             }
         }
-        return count==numCourses;
-
+        return numCourses==count;
     }
 };
