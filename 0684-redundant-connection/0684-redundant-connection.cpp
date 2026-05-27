d16be7e7d318be186bf018d09bff227be3a5844c
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-27 23:02:22 +0530

    Time: 0 ms (100%), Space: 13.1 MB (70.87%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,44 +1,57 @@
-class DSU{
-    public:
-    vector<int>rank,parent;
-    DSU(int n){
-        parent.resize(n+1);
-        rank.resize(n+1);
-        for(int i=1;i<=n;i++){
-            parent[i]=i;
-            rank[i]=0;
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x) {
+        if (x == parent[x]) {
+            return x;
         }
+
+        return parent[x] = find(parent[x]);
     }
-    int find(int x){
-        if(x==parent[x])return x;
-        return parent[x]= find(parent[x]);
-    }
-    void Union(int x,int y){
-        int x_parent=find(x);
-        int y_parent=find(y);
-        if(x_parent==y_parent)return ;
-        if(rank[x_parent]>rank[y_parent]){
-            parent[y_parent]=x_parent;
-        }else if(rank[x_parent]<rank[y_parent]){
-            parent[x_parent]=y_parent;
-        }else {
-            parent[y_parent]=x_parent;
+
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent) {
+            return;
+        }
+
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        }
+        else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        }
+        else {
+            parent[y_parent] = x_parent;
             rank[x_parent]++;
         }
     }
-};
-class Solution {
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-       int n=edges.size() ;
-       DSU dsu(n);
-       for(auto&edge:edges){
-        int u=edge[0];
-        int v=edge[1];
 
-        if(dsu.find(u)==dsu.find(v))return {u,v};
-        dsu.Union(u,v);
-       }
-       return {};
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (find(u) == find(v)) {
+                return {u, v};
+            }
+
+            Union(u, v);
+        }
+
+        return {};
     }
 };
