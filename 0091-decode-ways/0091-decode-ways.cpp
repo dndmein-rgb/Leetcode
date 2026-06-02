75ec49084d0f9752ca67c21a775c884e54ebde1a
Divyanshu Rathore <dnd.mein@gmail.com>
2026-06-02 14:40:35 +0530

    Time: 0 ms (100%), Space: 8.7 MB (50.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-int n;
-int t[101];
-int solve(int i,string &s){
-    if(i==n)return 1;
-    if(t[i]!=-1)return t[i];
-    if(s[i]=='0')return 0;
-
-    int ways=solve(i+1,s);
-    if(i+1<n){
-        if(s[i]=='1' ||( s[i]=='2' && s[i+1]<='6')){
-            ways+=solve(i+2,s);
-        }
-    }
-    return t[i]=ways;
-}
     int numDecodings(string s) {
-      n=s.length()  ;
-    memset(t,-1,sizeof(t));
-    return solve(0,s);
+        int n = s.length();
+        vector<int> t(n + 1, 0);
+        t[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '0')
+                t[i] = 0;
+            else {
+                t[i] = t[i + 1];
+
+                if (i < n - 1 &&
+                    (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
+                    t[i] += t[i + 2];
+                }
+            }
+        }
+        return t[0];
     }
 };
