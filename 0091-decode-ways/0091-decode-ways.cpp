d323b7d4e92394f7d08ad22e27b78d7bd0511db0
Divyanshu Rathore <dnd.mein@gmail.com>
2026-06-02 14:22:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (79.69%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int n;
+int t[101];
+int solve(int i,string &s){
+    if(t[i]!=-1)return t[i];
+    if(i==n)return t[i]=1;
+    if(s[i]=='0')return t[i]=0;
+
+    int ways=solve(i+1,s);
+    if(i+1<n){
+        if(s[i]=='1' ||( s[i]=='2' && s[i+1]<='6')){
+            ways+=solve(i+2,s);
+        }
+    }
+    return t[i]=ways;
+}
+    int numDecodings(string s) {
+      n=s.length()  ;
+    memset(t,-1,sizeof(t));
+    return solve(0,s);
+    }
+};
