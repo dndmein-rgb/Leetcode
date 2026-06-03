2f798ee4ce5a6a33b1bb406116017bdd1d41470b
Divyanshu Rathore <divyanshurathore0304@gmail.com>
2026-06-03 15:08:44 +0530

    Time: 2 ms (75.32%), Space: 10.9 MB (79.42%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-unordered_set<string>st;
-int t[301];
-int n;
-bool solve(int i,vector<string>&wordDict,string&s){
-    if(i==n)return true;
-    if(t[i]!=-1)return t[i];
-    string curr="";
-    for(int j=i;j<n;j++){
-        curr.push_back(s[j]);
-        if(st.count(curr)){
-            if(solve(j+1,wordDict,s))return t[i]=true;
-        }
-    }
-    return t[i]=false;
-}
     bool wordBreak(string s, vector<string>& wordDict) {
-        n=s.length();
-        st.insert(wordDict.begin(),wordDict.end());
-        memset(t,-1,sizeof(t));
-        return solve(0,wordDict,s);
+
+        int n = s.length();
+
+        unordered_set<string> st(
+            wordDict.begin(),
+            wordDict.end()
+        );
+
+        vector<bool> dp(n + 1, false);
+
+        dp[n] = true;
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            string curr = "";
+
+            for(int j = i; j < n; j++) {
+
+                curr.push_back(s[j]);
+
+                if(st.count(curr) && dp[j + 1]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[0];
     }
 };
