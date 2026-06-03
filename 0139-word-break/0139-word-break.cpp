32d527f0df51f200f2dd2295ec35772d22b0e734
Divyanshu Rathore <divyanshurathore0304@gmail.com>
2026-06-03 14:36:34 +0530

    Time: 4 ms (62.83%), Space: 12 MB (69.86%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+unordered_set<string>st;
+int t[1001];
+int n;
+bool solve(int i,vector<string>&wordDict,string&s){
+    if(i==n)return true;
+    if(t[i]!=-1)return t[i];
+    string curr="";
+    for(int j=i;j<n;j++){
+        curr.push_back(s[j]);
+        if(st.count(curr)){
+            if(solve(j+1,wordDict,s))return t[i]=true;
+        }
+    }
+    return t[i]=false;
+}
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n=s.length();
+        st.insert(wordDict.begin(),wordDict.end());
+        memset(t,-1,sizeof(t));
+        return solve(0,wordDict,s);
+    }
+};
