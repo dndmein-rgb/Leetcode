f5d6171a4b236ad00a00d80e5e0c7d856df98feb
Divyanshu Rathore <divyanshurathore0304@gmail.com>
2026-06-13 20:23:31 +0530

    Time: 1008 ms (5.02%), Space: 55.1 MB (46.13%) - LeetHub

--- 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
+++ 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
@@ -1,60 +1,36 @@
 class Solution {
 public:
-    int n;
-    vector<int> cnt;
-    int dp[1001][1501][2];
-
-    int helper(int i, int budget, int taken,
-               vector<vector<int>>& items) {
-
-        if (i == n) return 0;
-
-        int &ans = dp[i][budget][taken];
-        if (ans != -1) return ans;
-
-        ans = 0;
-
-        int price = items[i][1];
-
-        if (taken == 0) {
-
-            ans = helper(i + 1, budget, 0, items);
-
-            if (budget >= price) {
-                ans = max(ans,
-                          1 + cnt[i] +
-                          helper(i, budget - price, 1, items));
-            }
+int n;
+vector<int>cnt;
+int dp[1501][1501][2];
+int solve(int i,vector<vector<int>>& items, int budget,int taken){
+    if(i==n)return 0;
+    if(dp[i][budget][taken]!=-1)return dp[i][budget][taken];
+    int ans=solve(i+1,items,budget,0);
+    int price=items[i][1];
+    if(budget >=price){
+        if(taken){
+            ans=max(ans,1+solve(i,items,budget - price,1));
         }
         else {
-
-            ans = helper(i + 1, budget, 0, items);
-
-            if (budget >= price) {
-                ans = max(ans,
-                          1 +
-                          helper(i, budget - price, 1, items));
-            }
+            ans=max(ans,1+cnt[i]+solve(i,items,budget - price,1));
         }
-
-        return ans;
     }
+    return dp[i][budget][taken]=ans;
 
+
+}
     int maximumSaleItems(vector<vector<int>>& items, int budget) {
-
-        n = items.size();
-        cnt.assign(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j &&
-                    items[j][0] % items[i][0] == 0)
+        n=items.size();
+        cnt.assign(n,0);
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && items[j][0]%items[i][0]==0){
                     cnt[i]++;
+                }
             }
         }
-
-        memset(dp, -1, sizeof(dp));
-
-        return helper(0, budget, 0, items);
+        return solve(0,items,budget,0);
     }
 };
