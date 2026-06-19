ae4847a4d3f401eb08488366bba6dc957caf1fec
Divyanshu Rathore <divyanshurathore0304@gmail.com>
2026-06-19 08:43:57 +0530

    Time: 333 ms (80.38%), Space: 62.3 MB (70.67%) - LeetHub

--- 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
+++ 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
@@ -3,33 +3,39 @@ public:
     int n, m;
     int ans = INT_MAX;
 
-    vector<vector<bitset<1024>>> vis;
+    vector<vector<vector<bool>>> vis;
 
     void dfs(int i, int j, int xr, vector<vector<int>>& grid) {
         if (i >= n || j >= m) return;
 
         xr ^= grid[i][j];
 
-        // prune
         if (vis[i][j][xr]) return;
-        vis[i][j][xr] = 1;
+        vis[i][j][xr] = true;
 
-        if (i == n-1 && j == m-1) {
+        if (i == n - 1 && j == m - 1) {
             ans = min(ans, xr);
             return;
         }
 
-        dfs(i+1, j, xr, grid);
-        dfs(i, j+1, xr, grid);
+        dfs(i + 1, j, xr, grid);
+        dfs(i, j + 1, xr, grid);
     }
 
     int minCost(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
 
-        vis.assign(n, vector<bitset<1024>>(m));
+        vis.assign(
+            n,
+            vector<vector<bool>>(
+                m,
+                vector<bool>(1024, false)
+            )
+        );
 
         dfs(0, 0, 0, grid);
+
         return ans;
     }
 };
