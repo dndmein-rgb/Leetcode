e35097a6cbc72425eec46c2290be8341a7e2a82a
Divyanshu Rathore <divyanshurathore0304@gmail.com>
2026-07-04 08:03:13 +0530

    Time: 56 ms (68.65%), Space: 133 MB (66.06%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,48 +1,22 @@
-class DSU {
-public:
-    vector<int> parent, rank;
-
-    DSU(int n) {
-        parent.resize(n+1);
-        rank.resize(n+1);
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-    int find(int x) {
-        if (x == parent[x])
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-    void UNION(int x, int y) {
-        int x_parent = find(x);
-        int y_parent = find(y);
-        if (x_parent == y_parent)
-            return;
-
-        if (rank[x_parent] < rank[y_parent]) {
-            parent[x_parent] = y_parent;
-        } else if (rank[x_parent] > rank[y_parent]) {
-            parent[y_parent] = x_parent;
-        } else {
-            parent[y_parent] = x_parent;
-            rank[x_parent]++;
-        }
-    }
-};
 class Solution {
 public:
+int ans=INT_MAX;
+void dfs(int i,vector<vector<pair<int,int>>>&adj,vector<vector<int>>&roads,vector<bool>&vis){
+
+    vis[i]=true;
+    for(auto &[next, wt]:adj[i]){
+        ans=min(ans,wt);
+        if(!vis[next])dfs(next,adj,roads,vis);
+    }
+}
     int minScore(int n, vector<vector<int>>& roads) {
-        DSU dsu(n+1) ;
-        for(auto& r:roads){
-            dsu.UNION(r[0],r[1]);
+        vector<vector<pair<int,int>>>adj(n+1);
+        vector<bool>vis(n+1);
+        for(auto&r:roads){
+            adj[r[0]].push_back({r[1],r[2]});
+            adj[r[1]].push_back({r[0],r[2]});
         }
-        int root=dsu.find(1);
-        int  ans=INT_MAX;
-        for(auto& r:roads){
-            if(dsu.find(r[0])==root)ans=min(ans,r[2]);
-        }
-        return ans;
+         dfs(1,adj,roads,vis);
+         return ans;
     }
 };
