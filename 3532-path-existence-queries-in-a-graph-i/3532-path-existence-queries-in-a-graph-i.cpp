f36b7c92b2be73f26a10580ac66deed917d30eff
Divyanshu Rathore <divyanshurathore0304@gmail.com>
2026-07-09 06:59:48 +0530

    Time: 40 ms (34.25%), Space: 228.8 MB (55.07%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,37 +1,48 @@
+class DSU {
+public:
+    vector<int> parent, rank;
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+    void UNION(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if (x_parent == y_parent)
+            return;
+
+        if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else {
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+};
 class Solution {
 public:
-//  vector<int>parent,rank;
-//         int find(int x){
-//             if(x==parent[x])return x;
-//             return parent[x]=find(parent[x]);
-//         }
-//         void Union(int x, int y) {
-//         int x_parent = parent[x];
-//         int y_parent = parent[y];
-//         if (x_parent == y_parent)
-//             return;
-//         if (rank[x_parent] > rank[y_parent]) {
-//             parent[y_parent] = x_parent;
-//         } else if (rank[y_parent] > rank[x_parent]) {
-//             parent[x_parent] = y_parent;
-//         } else {
-//             parent[x_parent] = y_parent;
-//             rank[y_parent]++;
-//         }
-//     }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-       vector<int>comp(n,0);
-       for(int i=1;i<n;i++){
-        if(nums[i]-nums[i-1]<=maxDiff)
-        comp[i]=comp[i-1];
-       
-       else comp[i]=comp[i-1]+1;
-       }
+        DSU dsu(n);
+        for(int i=1;i<n;i++){
+            if((nums[i]-nums[i-1])<=maxDiff && dsu.find(i)!=dsu.find(i-1))dsu.UNION(i,i-1);
+        }
         vector<bool>ans;
         for(auto& q:queries){
-            int u=comp[q[0]];
-            int v=comp[q[1]];
-           ans.push_back(u==v);
+            int u=q[0];
+            int v=q[1];
+            ans.push_back(dsu.find(u)==dsu.find(v));
         }
         return ans;
     }
